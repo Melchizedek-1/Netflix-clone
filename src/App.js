b991328cf6f0dc89077e03646cc49eb25e818154
Melchizedek Wesonga <melchizedek.wesonga@skylabsystems.co.ke>
2023-02-15 08:01:31 +0300

    About us added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import AboutUs from './Screens/AboutUs'
 import HomeScreen from './Screens/HomeScreen'
 import NotFound from './Screens/NotFound'
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<HomeScreen />} />
+      <Route path='/about-us' element={<AboutUs />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
