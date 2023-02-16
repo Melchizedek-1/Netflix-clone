0cd2ea6ffc42565b8d93c938db4cea93a324807d
Melchizedek Wesonga <melchizedek.wesonga@skylabsystems.co.ke>
2023-02-16 12:59:35 +0300

    Register and login page added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Route, Routes } from 'react-router-dom'
 import AboutUs from './Screens/AboutUs'
 import ContactUs from './Screens/ContactUs'
 import HomeScreen from './Screens/HomeScreen'
+import Login from './Screens/Login'
 import MoviesPage from './Screens/MoviesPage'
 import NotFound from './Screens/NotFound'
+import Register from './Screens/Register'
 import SingleMovie from './Screens/SingleMovie'
 import WatchPage from './Screens/WatchPage'
 
@@ -12,6 +14,8 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       <Route path='/about-us' element={<AboutUs />} />
       <Route path='/contact-us' element={<ContactUs />} />
       <Route path='/movies' element={<MoviesPage />} />
